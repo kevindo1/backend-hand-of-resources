d4510212d6be54468e6a010a766e186e9086f03d
Kevin Do <kevind0501@gmail.com>
2022-03-18 16:19:14 -0700

    games test

--- __tests__/games.test.js
+++ __tests__/games.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Game = require('../lib/models/Games');
 
 describe('games routes', () => {
   beforeEach(() => {
@@ -21,5 +22,13 @@ describe('games routes', () => {
       name: 'Mario',
       system: 'Nintendo',
     });
+  });
+
+  it('should get list of games', async () => {
+    const game1 = await Game.insert({ name: 'Mario', system: 'Nintendo' });
+    const game2 = await Game.insert({ name: 'Luigi', system: 'Nintendo' });
+
+    const res = await request(app).get('/api/v1/games');
+    expect(res.body).toEqual([game1, game2]);
   });
 });
