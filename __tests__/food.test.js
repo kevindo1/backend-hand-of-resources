7ba4c9ca17510e1908d5ba45602cc7a2e416e245
Kevin Do <kevind0501@gmail.com>
2022-03-17 17:04:28 -0700

    food get controller

--- __tests__/food.test.js
+++ __tests__/food.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Food = require('../lib/models/Food');
 
 describe('food route', () => {
   beforeEach(() => {
@@ -12,7 +13,7 @@ describe('food route', () => {
     pool.end();
   });
 
-  it.only('should create a food', async () => {
+  it('should create a food', async () => {
     const expected = {
       id: expect.any(String),
       name: 'Burger',
@@ -25,5 +26,19 @@ describe('food route', () => {
       name: 'Burger',
       calories: '800',
     });
+  });
+
+  it('should get a list of food', async () => {
+    const food1 = await Food.insert({
+      name: 'Burger',
+      calories: '800',
+    });
+    const food2 = await Food.insert({
+      name: 'Salmon',
+      calories: '400',
+    });
+
+    const res = await request(app).get('/api/v1/food');
+    expect(res.body).toEqual(expect.arrayContaining([food1, food2]));
   });
 });
