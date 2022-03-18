3453fcc8a4732d3b1e0691e58add9b737803b7be
Kevin Do <kevind0501@gmail.com>
2022-03-18 11:03:39 -0700

    test anime insert

---
+++ __tests__/anime.test.js
@@ -0,0 +1,24 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('anime routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('should add anime', async () => {
+    const anime = {
+      name: 'Jujutsu Kaisen',
+      character: 'Nobara Kugisaki',
+    };
+    const res = await request(app).post('/api/v1/anime').send(anime);
+
+    expect(res.body).toEqual(anime);
+  });
+});
